207a6e41e5b02cc4468e85a587fed36e72064f44
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-08-26 18:47:07 +0900

    Bacic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["hirohiroki"] && password == ENV["6521"]
+    end
+  end
+
 end
