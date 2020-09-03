88b96646a1c8b263e42e55e68904fa8fafa1d691
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-03 17:56:53 +0900

    itemモデル・コントローラ、ActiveStorage作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
 
-  def index
+  def index #前回作成
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 end
