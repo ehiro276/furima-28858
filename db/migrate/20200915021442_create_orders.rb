c1ece7665ca93dd0a0d6659830d63b6cccc35e70
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-15 11:44:14 +0900

    orders・addressテーブル作成

---
+++ db/migrate/20200915021442_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string     :postcode,         null: false
+      t.integer    :prefecture_id,    null: false
+      t.string     :city,             null: false
+      t.string     :number,           null: false
+      t.string     :building
+      t.string     :telephone_number, null: false
+      t.integer    :order_id,         null: false, foreign_key:true
+      t.timestamps
+    end
+  end
+end
