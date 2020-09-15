fc21944b0179d1012d771e91be86399ad34bdc6f
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-15 12:52:47 +0900

    バリデーション設定02

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address < ApplicationRecord
   belongs_to :order
 
   with_options presence: true do
-    validates :postcode, :prefecture_id, :city, :number, :telephone_number
+    validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city, :number
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/}
   end
 end
