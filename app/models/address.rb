c8d818d44f090398b6bc340f0503f14b72258d43
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-15 11:59:38 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  
+  with_options presence: true do
+    validates :postcode, :prefecture_id, :city, :number, :telephone_number
+  end
 end
